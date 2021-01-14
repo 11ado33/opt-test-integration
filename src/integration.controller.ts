04070b53716ea9b85d6d196e92c01d06413c661b
Andrej <andrew@buildiro.com>
2021-01-14 17:03:44 +0100

    [Done] - Added missing commits from previous commit

--- src/integration.controller.ts
+++ src/integration.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpException, HttpService, Post } from '@nestjs/common'
+import { Body, Controller, HttpException, HttpService, Post, UseGuards } from '@nestjs/common'
 import { IntegrationService } from './integration.service'
-import Order from './request/opTigerApi/Order'
-import PartnerOrder from './request/partner/PartnerOrder'
+import Order from './dto/request/opTigerApi/Order'
+import PartnerOrder from './dto/request/partner/PartnerOrder'
 import { catchError, map } from 'rxjs/operators'
+import { OPTAuth } from './auth/optAuth/OPTAuth'
 
 @Controller('api')
 export class IntegrationController {
@@ -10,9 +11,11 @@ export class IntegrationController {
   }
 
   @Post('orders')
+  @UseGuards(OPTAuth)
   async postOrders(@Body() body: PartnerOrder) {
+    const order = (await Order.createOrder(body))
     const token = Buffer.from(`${process.env.OPT_USERNAME}:${process.env.OPT_PASSWORD}`).toString('base64')
-    return this.httpService.post(process.env.EP_OPT_ORDER + '/api/orders', (await Order.createOrder(body)), {
+    return this.httpService.post(process.env.EP_OPT_ORDER + '/api/orders', order, {
       headers: {
         Authorization: 'Basic '
           + token,
@@ -20,6 +23,9 @@ export class IntegrationController {
     }).pipe(map(response => response.data)).pipe(catchError(e => {
         throw new HttpException(e.response.data, e.response.status)
       },
-    ))
+    )).pipe(map(async () => {
+      await order.save()
+      console.log("Order saved")
+    }))
   }
 }
--- src/integration.module.ts
+++ src/integration.module.ts
@@ -4,11 +4,12 @@ import { IntegrationService } from './integration.service'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { getTypeORMConfig } from './config/typeOrmCOnfig'
+import { PollingService } from './polling.service'
 
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(getTypeORMConfig()), HttpModule],
   controllers: [IntegrationController],
-  providers: [IntegrationService],
+  providers: [IntegrationService, PollingService],
 })
 export class IntegrationModule {
 }
